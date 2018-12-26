cdd8de3b0edaa246a5cfe1a9a6185a1913d25251
Johan Hallstensson <johan.behe.lind@gmail.com>
2018-12-26 09:04:27 +0100

    Day 12 part 2

--- test/day_12_test.exs
+++ test/day_12_test.exs
@@ -20,19 +20,7 @@ defmodule Day12Test do
   ####. => #
   """
 
-  @test_initial_state %{
-    0 => 1,
-    3 => 1,
-    5 => 1,
-    8 => 1,
-    9 => 1,
-    16 => 1,
-    17 => 1,
-    18 => 1,
-    22 => 1,
-    23 => 1,
-    24 => 1
-  }
+  @test_initial_state MapSet.new([0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24])
   @test_win_states MapSet.new([
                      [0, 0, 0, 1, 1],
                      [0, 0, 1, 0, 0],
@@ -56,71 +44,67 @@ defmodule Day12Test do
     end
 
     test "generate" do
-      assert generate(@test_initial_state, @test_win_states) == %{
-               0 => 1,
-               4 => 1,
-               9 => 1,
-               15 => 1,
-               18 => 1,
-               21 => 1,
-               24 => 1
-             }
+      assert generate(@test_initial_state, @test_win_states) ==
+               MapSet.new([0, 4, 9, 15, 18, 21, 24])
     end
 
     test "gen_20" do
-      gen_20 = gen_20(@input)
-
-      assert gen_20 == %{
-               -2 => 1,
-               3 => 1,
-               4 => 1,
-               9 => 1,
-               10 => 1,
-               11 => 1,
-               12 => 1,
-               13 => 1,
-               17 => 1,
-               18 => 1,
-               19 => 1,
-               20 => 1,
-               21 => 1,
-               22 => 1,
-               23 => 1,
-               28 => 1,
-               30 => 1,
-               33 => 1,
-               34 => 1
-             }
-
-      assert Map.keys(gen_20) |> Enum.sum() == 325
+      assert generation(@input, 20) == 325
     end
 
     test "with input" do
-      assert gen_20(File.read!("test/fixtures/day12.txt"))
-             |> Map.keys()
-             |> Enum.sum() == 2767
+      assert generation(File.read!("test/fixtures/day12.txt"), 20) == 2767
     end
   end
 
-  defp gen_20(input) do
+  describe "part 2" do
+    test "with input" do
+      assert generation(File.read!("test/fixtures/day12.txt"), 50_000_000_000) ==
+               2_650_000_001_362
+    end
+  end
+
+  defp generation(input, generations) do
     {initial_state, win_states} = parse(input)
 
-    Enum.reduce(1..20, initial_state, fn _, state ->
-      generate(state, win_states)
+    Enum.reduce_while(1..generations, {initial_state, sum(initial_state), [nil, nil, 0]}, fn
+      i, {state, sum, previous_diffs} ->
+        new_state = generate(state, win_states)
+        new_sum = sum(new_state)
+        new_diff = new_sum - sum
+
+        if stable_diff?(previous_diffs, new_diff) do
+          {:halt, {nil, (generations - i) * new_diff + new_sum, nil}}
+        else
+          {:cont, {new_state, new_sum, [new_diff | Enum.take(previous_diffs, 2)]}}
+        end
     end)
+    |> elem(1)
+  end
+
+  def stable_diff?(previous_diffs, new_diff) do
+    case [new_diff, new_diff, new_diff] do
+      ^previous_diffs -> true
+      _ -> false
+    end
+  end
+
+  defp sum(state) do
+    state
+    |> MapSet.to_list()
+    |> Enum.sum()
   end
 
   defp generate(state, win_states) do
-    {{min, _}, {max, _}} = Enum.min_max(state)
-
+    {min, max} = Enum.min_max(state)
     range = (min - 4)..(max + 4)
 
-    Enum.map(range, fn index -> Map.get(state, index, 0) end)
+    Enum.map(range, fn index -> if(MapSet.member?(state, index), do: 1, else: 0) end)
     |> Enum.chunk_every(5, 1, :discard)
     |> Enum.with_index(min - 2)
-    |> Enum.reduce(%{}, fn {value, index}, acc ->
+    |> Enum.reduce(MapSet.new(), fn {value, index}, acc ->
       if MapSet.member?(win_states, value) do
-        Map.put(acc, index, 1)
+        MapSet.put(acc, index)
       else
         acc
       end
@@ -133,8 +117,8 @@ defmodule Day12Test do
     initial =
       to_charlist(initial_state)
       |> Enum.with_index()
-      |> Enum.reduce(%{}, fn
-        {?#, index}, acc -> Map.put(acc, index, 1)
+      |> Enum.reduce(MapSet.new(), fn
+        {?#, index}, acc -> MapSet.put(acc, index)
         _, acc -> acc
       end)
 
