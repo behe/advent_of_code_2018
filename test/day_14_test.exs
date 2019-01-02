4496f7cf4ab96d871cb1bafab365546c1bdcde74
Johan Hallstensson <johan.behe.lind@gmail.com>
2019-01-02 21:06:22 +0100

    Day 14 part 2

--- test/day_14_test.exs
+++ test/day_14_test.exs
@@ -3,16 +3,16 @@ defmodule Day14Test do
 
   describe "part 1" do
     test "mix" do
-      assert mix(%{0 => 3, 1 => 7}, 0, 1) == {%{0 => 3, 1 => 7, 2 => 1, 3 => 0}, 0, 1}
+      assert mix(%{0 => 3, 1 => 7}, 2, 0, 1) == {%{0 => 3, 1 => 7, 2 => 1, 3 => 0}, 4, 0, 1, [1, 0]}
 
-      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0}, 0, 1) ==
-               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0}, 4, 3}
+      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0}, 4,0, 1) ==
+               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0}, 6,4, 3, [1, 0]}
 
-      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0}, 4, 3) ==
-               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1}, 6, 4}
+      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0}, 6,4, 3) ==
+               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1}, 7,6, 4, [1]}
 
-      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1}, 6, 4) ==
-               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1, 7 => 2}, 0, 6}
+      assert mix(%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1}, 7,6, 4) ==
+               {%{0 => 3, 1 => 7, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 1, 7 => 2}, 8,0, 6, [2]}
     end
 
     test "scores_after" do
@@ -27,35 +27,77 @@ defmodule Day14Test do
     end
   end
 
-  defp mix(map, first_position, second_position) do
-    first = Map.get(map, first_position)
-    second = Map.get(map, second_position)
-    sum = first + second
-    map_size = map_size(map)
+  describe "part 2" do
+    test "iterations_before" do
+      assert iterations_before([0, 1, 2, 4, 5]) == 5
+      assert iterations_before([5, 1, 5, 8, 9]) == 9
+      assert iterations_before([9, 2, 5, 1, 0]) == 18
+      assert iterations_before([5, 9, 4, 1, 4]) == 2018
+    end
 
-    scores =
-      case div(sum, 10) do
-        1 -> %{map_size => 1, (map_size + 1) => rem(sum, 10)}
-        0 -> %{map_size => rem(sum, 10)}
-      end
+    test "with input" do
+      assert iterations_before([5, 0, 3, 7, 6, 1]) == 20_185_425
+    end
+  end
 
-    map = Map.merge(map, scores)
-    first_position = next_position(first_position, first, map)
-    second_position = next_position(second_position, second, map)
-    {map, first_position, second_position}
+  defp iterations_before(scores) do
+    iterations_before({%{0 => 3, 1 => 7}, 2, 0, 1, [3, 7]}, [], scores)
+  end
+
+  defp iterations_before(
+         {map, map_size, first_position, second_position, added_scores},
+         current_scores,
+         scores
+       ) do
+    current_scores = current_scores ++ added_scores
+
+    current_scores =
+      Enum.drop(current_scores, max(length(current_scores) - length(scores) - 1, 0))
+
+    cond do
+      tl(current_scores) == scores ->
+        map_size - length(scores)
+
+      List.starts_with?(current_scores, scores) ->
+        map_size - length(scores) - 1
+
+      :otherwise ->
+        iterations_before(mix(map, map_size, first_position, second_position), current_scores, scores)
+    end
   end
 
   defp scores_after(iterations) do
-    Enum.reduce_while(1..(iterations + 10), {%{0 => 3, 1 => 7}, 0, 1}, fn
-      _, {map, _, _} when map_size(map) >= iterations + 10 ->
+    Enum.reduce_while(1..(iterations + 10), {%{0 => 3, 1 => 7}, 2, 0, 1, []}, fn
+      _, {map, map_size, _, _, _} when map_size >= iterations + 10 ->
         {:halt, Map.take(map, iterations..(iterations + 9)) |> Map.values()}
 
-      _, {map, first_position, second_position} ->
-        {:cont, mix(map, first_position, second_position)}
+      _, {map, map_size, first_position, second_position, _} ->
+        {:cont, mix(map, map_size, first_position, second_position)}
     end)
   end
 
-  defp next_position(position, score, map) when is_map(map) do
-    rem(position + score + 1, map_size(map))
+  defp mix(map, map_size, first_position, second_position) do
+    first = Map.get(map, first_position)
+    second = Map.get(map, second_position)
+    sum = first + second
+
+    scores =
+      case div(sum, 10) do
+        1 -> [1, rem(sum, 10)]
+        0 -> [rem(sum, 10)]
+      end
+
+    {map, map_size} =
+      Enum.reduce(scores, {map, map_size}, fn score, {acc, i} ->
+        {Map.put(acc, i, score), i + 1}
+      end)
+
+    first_position = next_position(first_position, first, map_size)
+    second_position = next_position(second_position, second, map_size)
+    {map, map_size, first_position, second_position, scores}
+  end
+
+  defp next_position(position, score, map_size) do
+    rem(position + score + 1, map_size)
   end
 end
